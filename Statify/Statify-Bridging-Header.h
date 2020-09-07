88f58705481997ed5ba6912da21f8bcfca53ecea
Hannes Simon <hannes.simon@tum.de>
2020-09-07 14:11:42 +0200

    add bridging header

---
+++ Statify/Statify-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Statify-Bridging-Header.h
+//  Statify
+//
+//  Created by Hannes Simon on 07.09.20.
+//  Copyright © 2020 Hannes. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
