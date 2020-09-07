613cff6c0e850463764904f088b88a2c19db073a
Hannes Simon <hannes.simon@tum.de>
2020-09-07 13:49:54 +0200

    create xcode project

---
+++ Statify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Statify
+//
+//  Created by Hannes Simon on 07.09.20.
+//  Copyright © 2020 Hannes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
